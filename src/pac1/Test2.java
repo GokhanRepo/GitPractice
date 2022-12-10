833702e592860228a9e26977aaecc70739ed3e9f
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2022-12-10 19:41:12 +0300

    updated Test 2 and 5.java file

--- src/pac1/Test2.java
+++ src/pac1/Test2.java
@@ -4,6 +4,8 @@ public class Test2 {
     public static void main(String[] args) {
         
         //finished updating Test2
+        //naming branches => day_12_10_creating_Test6
+        // also can use ticket names/user stories like avs-125
         
 
     }
--- src/pac1/Test5.java
+++ src/pac1/Test5.java
@@ -4,3 +4,6 @@ public class Test5 {
   
   // I finished my code, you can go on with your part
 }
+
+/*We did update after fetching. According to your diagram we do PULL after fetching. SO does UPDATE include both Fetch and PULL?
+ */
